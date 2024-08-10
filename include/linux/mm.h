2a5434982acb8685a3fcbaac94611aaaa1cf9966
Santhosh <santhosh.user.why.red@gmail.com>
2024-08-10 11:51:25 +0530

    mm: Adapt le9 patch for linux 4.4
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -86,6 +86,10 @@ extern int mmap_rnd_compat_bits __read_mostly;
 #define mm_forbids_zeropage(X)	(0)
 #endif
 
+extern unsigned long sysctl_anon_min_kbytes;
+extern unsigned long sysctl_clean_low_kbytes;
+extern unsigned long sysctl_clean_min_kbytes;
+
 extern unsigned long sysctl_user_reserve_kbytes;
 extern unsigned long sysctl_admin_reserve_kbytes;
 
